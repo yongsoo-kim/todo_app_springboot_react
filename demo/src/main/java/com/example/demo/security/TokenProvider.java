1bc7c9a3af46adb295988432041df7efefeefda7
yongsoo-kim <yongsookim.com@gmail.com>
2022-01-26 08:17:00 +0900

    feature: Add front end codes.

--- demo/src/main/java/com/example/demo/security/TokenProvider.java
+++ demo/src/main/java/com/example/demo/security/TokenProvider.java
@@ -2,7 +2,6 @@ package com.example.demo.security;
 
 import com.example.demo.model.UserEntity;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.time.Instant;
