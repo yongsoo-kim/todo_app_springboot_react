902ba8e932beb02b91dfa9ed83499bc65c5b46e2
yongsoo-kim <yongsookim.com@gmail.com>
2022-01-30 20:54:51 +0900

    feature: aws front side fix.

--- react-workspace/todo-react-app/src/app-config.js
+++ react-workspace/todo-react-app/src/app-config.js
@@ -4,6 +4,8 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
     backendHost = "http://localhost:8080"
+} else {
+    backendHost = "http://yong-prod-todo-backend.ap-northeast-1.elasticbeanstalk.com"
 }
 
 
